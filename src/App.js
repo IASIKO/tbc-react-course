8144c92d2b4976432e7db0e7b71e92f1ae644ae5
IASIKO <iaseshviligi@gmail.com>
2024-03-09 17:25:23 +0400

    Save footer

--- src/App.js
+++ src/App.js
@@ -62,10 +62,69 @@ function App() {
                 </li>
               </ul>
             </div>
-            <div style={{ color: "orange" }}>Cart</div>
+            <div style={{ color: "var(--red)" }}>Cart</div>
           </div>
         </nav>
       </header>
+
+      <footer className="footerWraper">
+        <div className="footerContentContainer">
+          <div className="footerRow">
+            <div className="footerColumn">
+              <div>
+                <h2>My Account</h2>
+                <ul>
+                  <li>My Account</li>
+                  <li>Register</li>
+                  <li>Log in</li>
+                  <li>My Order</li>
+                </ul>
+              </div>
+            </div>
+            <div className="footerColumn">
+              <div>
+                <h2>Information</h2>
+                <ul>
+                  <li>About us</li>
+                  <li>Catalog</li>
+                  <li>Contact us</li>
+                  <li>Term & Conditions</li>
+                </ul>
+              </div>
+            </div>
+            <div className="footerColumn">
+              <div>
+                <h2>Quick Link</h2>
+                <ul>
+                  <li>New User</li>
+                  <li>Help Center</li>
+                  <li>Report Spam</li>
+                  <li>Faq's</li>
+                </ul>
+              </div>
+            </div>
+            <div className="footerColumn">
+              <div>
+                <h2>Newsletter Subscription</h2>
+                <input type="email" placeholder="Email..." />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footerRulesContainer">
+          <div className="rulesContainer">
+            <p>
+              Copyright © 2024 All rights reserved | This website is made by{" "}
+              <a
+                href="https://www.linkedin.com/in/giorgi-iaseshvili"
+                target="_blank"
+              >
+                Giorgi Iaseshvili
+              </a>
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
