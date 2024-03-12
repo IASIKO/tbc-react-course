7704ed50187e3112b253001f4f289baf68a91ae0
IASIKO <iaseshviligi@gmail.com>
2024-03-12 22:13:31 +0400

    fix style issue

--- src/components/LandingContent.jsx
+++ src/components/LandingContent.jsx
@@ -5,8 +5,8 @@ const LandingContent = () => {
     <main>
       <div className=" w-[100%] h-[100vh] bg-landing-bgImage bg-center bg-cover bg-no-repeat relative z-0">
         <div className="absolute top-0 right-0 bottom-0 left-0 opacity-40 bg-black"></div>
-        <div className="py-[30px] rotate-[-4deg] text-center max-w-[660px] m-auto">
-          <h1 className="textStroke absolute top-[40vh] text-[80px] text-8xl leading-tight font-bold italic">
+        <h1 className="textStroke absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[90px] leading-tight font-bold italic">
+          <div className="py-[30px] rotate-[-4deg] text-center w-[660px] m-auto">
             Good <span className="text-white">Drink</span> for Good{" "}
             <span className="text-white">Moments</span>.
             <p className="font-normal text-[20px] text-[#ffffff] py-[20px] leading-normal">
@@ -23,8 +23,8 @@ const LandingContent = () => {
                 Read more
               </a>
             </p>
-          </h1>
-        </div>
+          </div>
+        </h1>
       </div>
     </main>
   );
