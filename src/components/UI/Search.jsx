4a1c4d7df696f926d69d2a48e0eb8f26653c17b5
IASIKO <iaseshviligi@gmail.com>
2024-03-15 17:28:07 +0400

    Add Search component

--- src/components/UI/Search.jsx
+++ src/components/UI/Search.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div className='pt-[60px] flex justify-center'>
+    <div className="pt-[60px] flex justify-center">
+      <input
+        type="text"
+        placeholder="Search product..."
+        className="border px-[15px] rounded-md w-[400px] outline-none"
+      />
+      <button className="cursor-pointer ml-[5px] bg-[#b7472a] border-[#b7472a] text-white rounded-md px-[15px]">
         Search
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
