2c79ce7da8f80efb7f514e5abf4096f5fd70aaa2
IASIKO <iaseshviligi@gmail.com>
2024-03-15 19:59:08 +0400

    Save

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/Header";
-import LandingContent from "./components/LandingContent";
+// import LandingContent from "./components/LandingContent";
 import ProductsList from "./components/products/ProductsList";
 
 function App() {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      {/* <main className="overflow-auto"> */}
-      <LandingContent />
-      <ProductsList />
-      {/* </main> */}
+      <main className="overflow-auto">
+        {/* <LandingContent /> */}
+        <ProductsList />
+      </main>
       <Footer />
     </div>
   );
