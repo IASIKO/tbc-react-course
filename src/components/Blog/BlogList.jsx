64db966ee560c950755982209baa31be4059d719
IASIKO <iaseshviligi@gmail.com>
2024-03-25 21:05:01 +0400

    Add blog item component

--- src/components/Blog/BlogList.jsx
+++ src/components/Blog/BlogList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Blog from "./Blog";
 
 const blogListData = [
   {
@@ -145,24 +146,7 @@ const BlogList = () => {
         </div>
         <div className="w-[1140px] grid grid-cols-2 gap-4">
           {blogListData.map((listItem, index) => (
-            <div key={index} className="mb-[30px] flex">
-              <div
-                style={{ backgroundImage: `url(${listItem.image})` }}
-                className="w-[50%] bg-cover bg-no-repeat bg-center"
-              ></div>
-              <div className="w-[50%] bg-[#f5f4f0] p-[15px]">
-                <p className="mb-[10px]">
-                  <span className="pr-[10px] text-gray">{listItem.date}</span>
-                </p>
-                <h3 className="text-[27px] font-medium italic mb-[10px] leading-normal text-black">
-                  {listItem.title}
-                </h3>
-                <p className="mb-[10px]">{listItem.description}</p>
-                <a href="#" className="text-[#a23f25] italic font-bold">
-                  Continue
-                </a>
-              </div>
-            </div>
+            <Blog listItem={listItem} key={index} />
           ))}
         </div>
       </div>
