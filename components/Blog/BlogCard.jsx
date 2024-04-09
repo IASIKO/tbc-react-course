59a01797b0fa2da7e04c13f3016e32e6f7ea9d99
IASIKO <iaseshviligi@gmail.com>
2024-04-09 13:40:57 +0400

    Add api call on blog list page

--- components/Blog/BlogCard.jsx
+++ components/Blog/BlogCard.jsx
@@ -9,9 +9,9 @@ const BlogCard = ({ listItem }) => {
         className="w-[50%] bg-cover bg-no-repeat bg-center"
       ></div>
       <div className="w-[50%] bg-[#f5f4f0] p-[15px]">
-        <p className="mb-[10px]">
+        <p className="mb-[10px] flex items-center">
           <MdStarRate />
-          <span className="pr-[10px] text-gray">{listItem.rating}</span>
+          <span className="pl-[10px] text-gray">{listItem.rating}</span>
         </p>
         <h3 className="text-[27px] font-medium italic mb-[10px] leading-normal text-black">
           {listItem.name}
