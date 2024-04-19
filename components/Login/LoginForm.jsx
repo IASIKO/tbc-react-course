168295fc636b0457b53707e469c4d3cbfe569eff
IASIKO <iaseshviligi@gmail.com>
2024-04-19 21:08:02 +0400

    add dark mode logic, with saving in localsorage

--- components/Login/LoginForm.jsx
+++ components/Login/LoginForm.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { useFormStatus } from "react-dom";
+import Theme from "../UI/Theme";
 
 const LoginForm = ({ handleLogin }) => {
   const [loginInfo, setLoginInfo] = useState({
@@ -11,59 +12,62 @@ const LoginForm = ({ handleLogin }) => {
   const { pending } = useFormStatus();
 
   return (
-    <form
-      className="w-full flex flex-col justify-center items-center px-[90px]"
-      action={() => {
-        handleLogin(loginInfo.username, loginInfo.password);
-      }}
-    >
-      <h2 className="uppercase tracking-widest mb-3">acount login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        required
-        value={loginInfo.username}
-        onChange={(e) =>
-          setLoginInfo((prevState) => ({
-            ...prevState,
-            username: e.target.value,
-          }))
-        }
-        className="w-full outline-none py-[5px] px-[15px] border border-red mb-3"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        required
-        value={loginInfo.password}
-        onChange={(e) =>
-          setLoginInfo((prevState) => ({
-            ...prevState,
-            password: e.target.value,
-          }))
-        }
-        className="w-full outline-none py-[5px] px-[15px] border border-red mb-3"
-      />
-      <button
-        type="submit"
-        disabled={pending}
-        className="uppercase bg-red w-full py-[5px] text-white mb-3 ease-in duration-300 hover:bg-lightred"
+    <div className="w-full flex flex-col items-center py-8 gap-40">
+      <Theme />
+      <form
+        className="w-full flex flex-col justify-center items-center px-[90px]"
+        action={() => {
+          handleLogin(loginInfo.username, loginInfo.password);
+        }}
       >
-        sign in
-      </button>
-      <p className="text-[18px]">
-        Forgot{" "}
-        <span className="text-red hover:text-black cursor-pointer ease-in duration-300">
-          Email / password?
-        </span>
-      </p>
-      <button
-        type="button"
-        className="uppercase text-red mt-[200px] cursor-pointer hover:text-black ease-in duration-300"
-      >
-        sign up
-      </button>
-    </form>
+        <h2 className="uppercase tracking-widest mb-3">acount login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          required
+          value={loginInfo.username}
+          onChange={(e) =>
+            setLoginInfo((prevState) => ({
+              ...prevState,
+              username: e.target.value,
+            }))
+          }
+          className="w-full outline-none py-[5px] px-[15px] border border-red mb-3"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={loginInfo.password}
+          onChange={(e) =>
+            setLoginInfo((prevState) => ({
+              ...prevState,
+              password: e.target.value,
+            }))
+          }
+          className="w-full outline-none py-[5px] px-[15px] border border-red mb-3"
+        />
+        <button
+          type="submit"
+          disabled={pending}
+          className="uppercase bg-red w-full py-[5px] text-white mb-3 ease-in duration-300 hover:bg-lightred"
+        >
+          sign in
+        </button>
+        <p className="text-[18px]">
+          Forgot{" "}
+          <span className="text-red hover:text-black cursor-pointer ease-in duration-300">
+            Email / password?
+          </span>
+        </p>
+        <button
+          type="button"
+          className="uppercase text-red mt-[200px] cursor-pointer hover:text-black ease-in duration-300"
+        >
+          sign up
+        </button>
+      </form>
+    </div>
   );
 };
 
--- components/UI/Theme.jsx
+++ components/UI/Theme.jsx
@@ -1,42 +1,87 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MdLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
-import { PiMonitorBold } from "react-icons/pi";
+import { MdMonitor } from "react-icons/md";
+
+const options = [
+  {
+    icon: <MdLightMode />,
+    text: "light",
+  },
+  {
+    icon: <MdDarkMode />,
+    text: "dark",
+  },
+  {
+    icon: <MdMonitor />,
+    text: "system",
+  },
+];
 
 const Theme = () => {
-  const [theme, setTheme] = useState("system");
-  const [themeIcon, setThemeIcon] = useState(<MdLightMode />);
-  const [themeModal, setThemeModal] = useState(false);
+  const [theme, setTheme] = useState(() =>
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "system"
+  );
+  const element = document.documentElement;
+  const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
-  const themeHandleClick = () => {
-    setThemeModal(!themeModal);
+  const onWindowMatch = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && darkQuery.matches)
+    ) {
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("dark");
+    }
   };
 
+  onWindowMatch();
+
+  useEffect(() => {
+    switch (theme) {
+      case "dark":
+        element.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+        break;
+
+      case "light":
+        element.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        break;
+
+      default:
+        localStorage.removeItem("theme");
+        onWindowMatch();
+        break;
+    }
+  }, [theme]);
+
+  darkQuery.addEventListener("change", (e) => {
+    if ("theme" in localStorage) {
+      if (e.mathes) {
+        element.classList.add("dark");
+      } else {
+        element.classList.remove("dark");
+      }
+    }
+  });
+
   return (
-    <div className="flex items-center">
-      <button className="text-white" onClick={themeHandleClick}>
-        {themeIcon}
-      </button>
-      {themeModal && (
-        <div className="absolute top-[50px] right-[60px] z-20">
-          <div className="bg-white py-1 rounded-md">
-            <button className="w-full flex items-center gap-3 px-[10px] pr-10 hover:bg-secondary">
-              <MdLightMode />
-              <span className="text-black">Light</span>
-            </button>
-            <button className="w-full flex items-center gap-3 px-[10px] pr-10 hover:bg-secondary">
-              <MdDarkMode />
-              <span className="text-black">Dark</span>
-            </button>
-            <button className="w-full flex items-center gap-3 px-[10px] pr-10 hover:bg-secondary">
-              <PiMonitorBold />
-              <span className="text-black">System</span>
-            </button>
-          </div>
-        </div>
-      )}
+    <div className="duration-100 dark:bg-slate-700 bg-gray rounded">
+      {options.map((opt) => (
+        <button
+          className={`w-8 h-8 align-middle leading-9 text-xl rounded-full m-1 text-white ${
+            theme === opt.text && "text-yellow"
+          }`}
+          onClick={() => setTheme(opt.text)}
+          key={opt.text}
+        >
+          {opt.icon}
+        </button>
+      ))}
     </div>
   );
 };
