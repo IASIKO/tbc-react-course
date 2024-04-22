c8c15991ac8857fd952f3f12a296104a49bb1568
IASIKO <iaseshviligi@gmail.com>
2024-04-22 15:13:29 +0400

    Update changes

--- components/UI/Language.jsx
+++ components/UI/Language.jsx
@@ -2,21 +2,31 @@
 
 import { setLanguage } from "@/lib/actions";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Language = ({ dict }) => {
+  let pathname = usePathname();
+  let route = pathname.substring(6);
+  const isEnUs = pathname.startsWith("/en-US");
+
   const langHandleCLick = (lang) => {
     setLanguage(lang);
-  }
-
+  };
 
   return (
     <div className="flex gap-4 items-center justify-center">
-      <Link href={`/en-US`} className="dark:text-yellow">
+      <Link
+        href={`/en-US/${route}`}
+        className={isEnUs ? "text-yellow" : "text-black dark:text-balck"}
+      >
         <button onClick={() => langHandleCLick("en-US")}>
           {dict.login.en}
         </button>
       </Link>
-      <Link href={`/ka-GE`} className="dark:text-yellow">
+      <Link
+        href={`/ka-GE/${route}`}
+        className={!isEnUs ? "text-yellow" : "text-black dark:text-black"}
+      >
         <button onClick={() => langHandleCLick("ka-GE")}>
           {dict.login.ge}
         </button>
