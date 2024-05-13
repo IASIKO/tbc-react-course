188689340ff92c5e7ee85e2f0c910ba5e386202a
IASIKO <iaseshviligi@gmail.com>
2024-05-13 15:47:48 +0400

    update ui

--- components/UI/AuthorizationLayout.tsx
+++ components/UI/AuthorizationLayout.tsx
@@ -8,7 +8,7 @@ interface AuthorizationLayoutProps {
 
 const AuthorizationLayout: React.FC<AuthorizationLayoutProps> = ({ children }) => {
   return (
-    <main className="bg-[#f5f4f0] w-screen h-screen dark:bg-dark">
+    <main className="bg-[#f5f4f0] dark:bg-dark">
       <div className="bg-white w-[1100px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 dark:bg-gray">
         <div className="flex">
           <Image src={img} alt="login-image" placeholder="blur" width={550}/>
