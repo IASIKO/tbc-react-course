60696e57fbd60a135d4a7f4e5c2ec734b086b350
IASIKO <iaseshviligi@gmail.com>
2024-05-16 12:01:34 +0400

    save

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,7 +1,6 @@
 import { unstable_setRequestLocale } from "next-intl/server";
 import LoginForm from "../../../components/Login/LoginForm";
 import AuthorizationLayout from "../../../components/UI/AuthorizationLayout";
-import { BASE_URL } from "../../../lib/api";
 
 export default async function Login({
   params: { locale },
