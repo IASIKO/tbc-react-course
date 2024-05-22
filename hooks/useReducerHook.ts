3c8ff44d717b7aaca533465cece5590a6b48356f
IASIKO <iaseshviligi@gmail.com>
2024-05-22 23:53:07 +0400

    save

--- hooks/useReducerHook.ts
+++ hooks/useReducerHook.ts
@@ -1,86 +1,86 @@
-import { useReducer } from "react";
-import { Product, selectedProduct } from "../types/products-types";
+// import { useReducer } from "react";
+// import { Product, selectedProduct } from "../types/products-types";
 
-const initialState: selectedProduct[] = [];
+// const initialState: selectedProduct[] = [];
 
-type Action =
-  | { type: "INCREMENT"; payload: Product }
-  | { type: "DECREMENT"; payload: Product }
-  | { type: "RESET"; payload: Product }
-  | { type: "CLEAR" };
+// type Action =
+//   | { type: "INCREMENT"; payload: Product }
+//   | { type: "DECREMENT"; payload: Product }
+//   | { type: "RESET"; payload: Product }
+//   | { type: "CLEAR" };
 
-const reducer = (state: selectedProduct[], action: Action) => {
-  switch (action.type) {
-    case "INCREMENT": {
-      const selectedProductsIndex = state.findIndex(
-        (p) => p.product?.id === action.payload.id
-      );
+// const reducer = (state: selectedProduct[], action: Action) => {
+//   switch (action.type) {
+//     case "INCREMENT": {
+//       const selectedProductsIndex = state.findIndex(
+//         (p) => p.product?.id === action.payload.id
+//       );
 
-      if (selectedProductsIndex === -1)
-        return [...state, { product: action.payload, count: 1 }];
+//       if (selectedProductsIndex === -1)
+//         return [...state, { product: action.payload, count: 1 }];
 
-      const clone = [...state];
-      const selectedProduct = clone[selectedProductsIndex];
-      const updatedSelectedProduct = {
-        ...selectedProduct,
-        count: selectedProduct.count + 1,
-      };
-      clone[selectedProductsIndex] = updatedSelectedProduct;
+//       const clone = [...state];
+//       const selectedProduct = clone[selectedProductsIndex];
+//       const updatedSelectedProduct = {
+//         ...selectedProduct,
+//         count: selectedProduct.count + 1,
+//       };
+//       clone[selectedProductsIndex] = updatedSelectedProduct;
 
-      return clone;
-    }
+//       return clone;
+//     }
 
-    case "DECREMENT": {
-      const selectedProductsIndex = state.findIndex(
-        (p) => p.product?.id === action.payload.id
-      );
+//     case "DECREMENT": {
+//       const selectedProductsIndex = state.findIndex(
+//         (p) => p.product?.id === action.payload.id
+//       );
 
-      if (selectedProductsIndex === -1)
-        return [...state, { product: action.payload, count: 1 }];
+//       if (selectedProductsIndex === -1)
+//         return [...state, { product: action.payload, count: 1 }];
 
-      const clone = [...state];
-      const selectedProduct = clone[selectedProductsIndex];
-      const updatedSelectedProduct = {
-        ...selectedProduct,
-        count: selectedProduct.count - 1,
-      };
-      if (updatedSelectedProduct.count === 0) {
-        clone.splice(selectedProductsIndex, 1);
-      } else {
-        clone[selectedProductsIndex] = updatedSelectedProduct;
-      }
+//       const clone = [...state];
+//       const selectedProduct = clone[selectedProductsIndex];
+//       const updatedSelectedProduct = {
+//         ...selectedProduct,
+//         count: selectedProduct.count - 1,
+//       };
+//       if (updatedSelectedProduct.count === 0) {
+//         clone.splice(selectedProductsIndex, 1);
+//       } else {
+//         clone[selectedProductsIndex] = updatedSelectedProduct;
+//       }
 
-      return clone;
-    }
+//       return clone;
+//     }
 
-    case "RESET": {
-      const productId = action.payload.id;
-      const selectedProductsIndex = state.findIndex(
-        (p) => p.product?.id === productId
-      );
-      if (selectedProductsIndex !== -1) {
-        const newState = [...state];
-        newState.splice(selectedProductsIndex, 1);
-        return newState;
-      }
-      return state;
-    }
+//     case "RESET": {
+//       const productId = action.payload.id;
+//       const selectedProductsIndex = state.findIndex(
+//         (p) => p.product?.id === productId
+//       );
+//       if (selectedProductsIndex !== -1) {
+//         const newState = [...state];
+//         newState.splice(selectedProductsIndex, 1);
+//         return newState;
+//       }
+//       return state;
+//     }
 
-    case "CLEAR":
-      return initialState;
-  }
-};
+//     case "CLEAR":
+//       return initialState;
+//   }
+// };
 
-export const useReducerHook = (): [
-  selectedProduct[],
-  React.Dispatch<Action>
-] => {
-  const [selectedProducts, dispatch] = useReducer(reducer, [], () => {
-    if (typeof window !== "undefined") {
-      const storedValue = window.localStorage.getItem("selectedProducts");
-      return storedValue ? JSON.parse(storedValue) : initialState;
-    }
-  });
+// export const useReducerHook = (): [
+//   selectedProduct[],
+//   React.Dispatch<Action>
+// ] => {
+//   const [selectedProducts, dispatch] = useReducer(reducer, [], () => {
+//     if (typeof window !== "undefined") {
+//       const storedValue = window.localStorage.getItem("selectedProducts");
+//       return storedValue ? JSON.parse(storedValue) : initialState;
+//     }
+//   });
 
-  return [selectedProducts, dispatch];
-};
+//   return [selectedProducts, dispatch];
+// };
