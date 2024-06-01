9311284ced1395519ea11f2b031df6aaf84850ab
IASIKO <iaseshviligi@gmail.com>
2024-06-01 21:43:25 +0400

    update deployment tasks

--- components/UI/LogoutButton.tsx
+++ components/UI/LogoutButton.tsx
@@ -1,19 +1,18 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { RiLogoutCircleLine } from "react-icons/ri";
-import { handleLogoutRoute } from "../../lib/helpers";
 import { useTranslations } from "next-intl";
 
 
 const LogoutButton = () => {
-  const router = useRouter();
+  // const router = useRouter();
   const t = useTranslations('header')
   return (
     <button
       className="text-red text-[20px] bg-white rounded-xl px-[10px] flex items-center gap-2"
       onClick={() => {
-        handleLogoutRoute().then(() => router.push("/login"));
+        // handleLogoutRoute().then(() => router.push("/login"));
       }}
     >
       <RiLogoutCircleLine />
