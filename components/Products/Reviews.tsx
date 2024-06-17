6f53e4b971a69153a594cef48341eddc0a846ee9
IASIKO <iaseshviligi@gmail.com>
2024-06-17 20:51:37 +0400

    fix language change bug

--- components/Products/Reviews.tsx
+++ components/Products/Reviews.tsx
@@ -183,7 +183,7 @@ const Reviews: React.FC<ReviewsProps> = ({
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
                   onClick={isClose}
-                  className="fixed inset-0 z-30 bg-[#000000bf] bg-opacity-90 flex items-center justify-center"
+                  className="fixed inset-0 z-30 bg-black bg-opacity-10 flex items-center justify-center"
                 >
                   <motion.div
                     initial={{ scale: 0, rotate: "12.5deg" }}
--- components/UI/Language.tsx
+++ components/UI/Language.tsx
@@ -6,34 +6,42 @@ import { useLocale } from "next-intl";
 import ge from "../../public/Assets/images/ge.svg";
 import gb from "../../public/Assets/images/gb.svg";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Language = () => {
   const router = useRouter();
   const locale = useLocale();
+  const [currentLocale, setCurrentLocale] = useState(locale);
 
-  const langHandleCLick = (lang: string) => {
+  const langHandleClick = (lang: string) => {
     setLanguage(lang);
+    setCurrentLocale(lang);
     router.refresh();
   };
 
+  // Update currentLocale if locale changes
+  useEffect(() => {
+    setCurrentLocale(locale);
+  }, [locale]);
+
   return (
     <div className="duration-100 flex gap-2 items-center justify-center">
-      <div className={locale === "en" ? "text-yellow" : "text-white"}>
+      <div className={currentLocale === "en" ? "text-yellow" : "text-white"}>
         <button
           type="button"
-          onClick={() => langHandleCLick("en")}
+          onClick={() => langHandleClick("en")}
           className="flex items-center"
         >
-          <Image src={ge} alt="Georgian flag" width={32} height={24} className="rounded hover:scale-125 duration-300"/>
+          <Image src={gb} alt="United Kingdom flag" width={28} height={21} className="rounded hover:scale-125 duration-300"/>
         </button>
       </div>
-      <div className={locale === "ka" ? "text-yellow" : "text-white"}>
+      <div className={currentLocale === "ka" ? "text-yellow" : "text-white"}>
         <button
           type="button"
-          onClick={() => langHandleCLick("ka")}
+          onClick={() => langHandleClick("ka")}
           className="flex items-center"
         >
-          <Image src={gb} alt="United Kingdom flag" width={32} height={24} className="rounded hover:scale-125 duration-300"/>
+          <Image src={ge} alt="Georgian flag" width={28} height={21} className="rounded hover:scale-125 duration-300"/>
         </button>
       </div>
     </div>
